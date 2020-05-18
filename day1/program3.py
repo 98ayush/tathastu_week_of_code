e3733e0414be7aba3875a768e416bfb17d44877d
98ayush <49728309+98ayush@users.noreply.github.com>
2020-05-19 01:28:39 +0530

    Create program3.py

---
+++ day1/program3.py
@@ -0,0 +1,8 @@
+num1=int(input("enter first number "))
+num2=int(input("enter second number "))
+num1=num1+num2
+num2=num1-num2
+num1=num1-num2
+print("Number after swipping are")
+print(num1)
+print(num2)
