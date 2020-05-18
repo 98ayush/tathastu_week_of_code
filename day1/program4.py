4231f87a47fe5299357f704c1f50d6d6bd520c4b
98ayush <49728309+98ayush@users.noreply.github.com>
2020-05-19 01:38:12 +0530

    Create program4.py

---
+++ day1/program4.py
@@ -0,0 +1,6 @@
+cost_price=float(input("enter the cost price "))
+selling_price=float(input("enter the selling price"))
+profit=selling_price-cost_price
+print("Profit is ",profit)
+new_selling_price=(105/100)*profit + cost_price
+print("After adding 5% profit selling price is ",new_selling_price)
